9cb16090aff5c2fe72f4421e43df6f8a5ae9d5c1
Matthieu Hertoghe <matthieu.hertoghe@chift.eu>
2023-06-21 14:40:03 +0200

    remove invoices from ecommerce

--- src/modules/ecommerce.ts
+++ src/modules/ecommerce.ts
@@ -74,22 +74,6 @@ const ecommerceFactory = {
             url: `/consumers/{consumer_id}/commerce/orders/${orderId}`,
         };
     },
-    getInvoicesByType(
-        invoice_type: components['schemas']['app__routers__invoicing__InvoiceType'],
-        params: operations['invoicing_get_invoices_by_type']['parameters']['query']
-    ): RequestData<components['schemas']['InvoiceItem'][]> {
-        return {
-            method: 'get',
-            url: `/consumers/{consumer_id}/invoicing/invoices/type/${invoice_type}`,
-            params: params,
-        };
-    },
-    getInvoice(invoiceId: string): RequestData<components['schemas']['InvoiceItem']> {
-        return {
-            method: 'get',
-            url: `/consumers/{consumer_id}/invoicing/invoices/${invoiceId}`,
-        };
-    },
 };
 
 export { ecommerceFactory };
