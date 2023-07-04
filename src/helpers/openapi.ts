8f4408b3e54ce2c8d5b05f20bde0327cfe5a5f69
Henry Hertoghe <henry.hertoghe@chift.eu>
2023-07-04 17:00:48 +0200

    new version

--- src/helpers/openapi.ts
+++ src/helpers/openapi.ts
@@ -10,6 +10,7 @@ export function createApiFor<TFactory extends RequestFactory>(
     return new Proxy(factory, {
         get(target: any, property) {
             return (...args: any[]) => {
+                console.log('hey');
                 const requestData = target[property](...args);
                 requestData.property = property;
                 requestData.consumerName = consumerName;
